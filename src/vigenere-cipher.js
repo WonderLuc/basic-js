03fd74f4e40eda84bd5931e726c2b65485663e55
wonderLuc <workmaildo@yandex.ru>
2021-03-15 21:31:51 +0500

    feat: complete vigenere-chiper task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -8,22 +8,89 @@ class VigenereCipheringMachine {
       this.direction = false;
     }
   }
-  encrypt(str, code) {
+
+  validation(str,code){
     if(!str || !code){
       throw new Error();
     }
+  }
+
+  encrypt(str, code) {
+    this.validation(str,code);
     str = str.toUpperCase();
+    code = code.toUpperCase();
+
+    // expand code to str length
     if(str.length != code.length){
-      let length = code.length;
+      let length;
       do{
-        code +=code;
+        code = code + code;
+        length = code.length;
       }
-      while(length < str.length);
-      code = [].slice.call(str,0,str.length+1).join('');
+      while(length < str.length);    
+      code = [].slice.call(code,0,str.length).join('');
     }
+
+    // chipher str 
+    let cipher = "";
+    for(let i =0, shift =0; i < str.length; i++){
+      if(!str[i].match(/[A-Z]/)){
+        cipher += str[i];
+        shift++
+        continue;
+      }
+
+      let letter = (str[i].charCodeAt(0)-64) + (code[i - shift].charCodeAt(0)-64) - 1;
+      if(letter > 26){
+        letter = letter-26;
+      }
+
+      cipher += String.fromCharCode(letter + 64);
+    }
+
+    if(!this.direction){
+      cipher = cipher.split('').reverse().join('');
+    }
+    return cipher;
   }    
+
   decrypt(str, code) {
-  
+    this.validation(str,code);
+    str = str.toUpperCase();
+    code = code.toUpperCase();
+    
+    // expand code to str length
+    if(str.length != code.length){
+      let length;
+      do{
+        code = code + code;
+        length = code.length;
+      }
+      while(length < str.length);    
+      code = [].slice.call(code,0,str.length).join('');
+    }
+    
+    let cipher = "";
+    for(let i =0, shift =0; i < str.length; i++){
+      if(!str[i].match(/[A-Z]/)){
+        cipher += str[i];
+        shift++
+        continue;
+      }
+
+      let letter = (str[i].charCodeAt(0)-64) - (code[i - shift].charCodeAt(0)-64);
+      if(letter < 0){
+        letter = letter+26;
+      }
+
+      cipher += String.fromCharCode(letter + 65);
+    }
+
+    if(!this.direction){
+      cipher = cipher.split('').reverse().join('');
+    }
+    return cipher;
+    
   }
 }
 
